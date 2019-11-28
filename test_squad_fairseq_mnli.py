ed5046f069a177021e6cd456643d952301826370
ecchochan <ecchochan@gmail.com>
2019-11-28 10:53:54 +0800

    Update test_squad_fairseq_mnli.py

--- test_squad_fairseq_mnli.py
+++ test_squad_fairseq_mnli.py
@@ -62,17 +62,16 @@ def char_anchors_to_tok_pos(r):
         
     return a, b
 
+import marshal
 def read(dat):
-    uid, inp, start, end, p_mask, unanswerable = marshal.loads(dat)
+    inp, label = marshal.loads(dat)
     inp = np.frombuffer(inp, dtype=np.uint16).astype(np.int32)
-    p_mask = np.frombuffer(p_mask, dtype=np.bool).astype(np.float32)
-    return uid, inp, start, end, p_mask, unanswerable
+    return inp, label
 
 def fread(f):
-    uid, inp, start, end, p_mask, unanswerable = marshal.load(f)
+    inp, label = marshal.load(f)
     inp = np.frombuffer(inp, dtype=np.uint16).astype(np.int32)
-    p_mask = np.frombuffer(p_mask, dtype=np.bool).astype(np.float32)
-    return uid, inp, start, end, p_mask, unanswerable
+    return inp, label
             
 def gen(paths):
     j = 0
@@ -90,138 +89,6 @@ def gen(paths):
         
         
 import marshal
-def work(ss, debug=False):
-    
-    unique_index, \
-     context, \
-     qas, \
-     is_training, \
-     return_feature = ss
-    
-    rss = tokenizer.merge_cq(context, 
-                             qas,
-                             max_seq_length = max_seq_length,
-                             max_query_length = max_query_length,
-                             doc_stride = doc_stride,
-                             default_choices = default_choices,
-                             unique_index=unique_index,
-                             is_training=is_training,
-                             merge_style=merge_style,
-                             debug = debug
-                           )
-    o = 0
-    results = []
-    for rs in rss:
-        q = qas[o]
-        o += 1
-        for r in rs:
-            inp = tk.convert_tokens_to_ids(r.all_doc_tokens)
-            start_position,end_position = char_anchors_to_tok_pos(r)
-            p_mask = r.p_mask
-            uid = r.unique_index[0]*1000 + r.unique_index[1]
-            if start_position == -1 and end_position == -1:
-                start_position = 0
-                end_position = 0
-                
-            
-            no_ans = start_position == 0
-            
-            #if no_ans:
-            #    print(q['answer_text'], '>>', r.all_doc_tokens[start_position:end_position+1])
-            assert start_position >= 0 and end_position >= 0 and start_position < len(inp) and end_position < len(inp)
-            assert len(inp) <= max_seq_length
-            record = marshal.dumps(
-                (
-                uid,
-                np.array(inp,dtype=np.uint16).tobytes(),
-                start_position,
-                end_position,
-                np.array(p_mask,dtype=np.bool).tobytes(),
-                int(no_ans)
-                )
-            )
-            
-            if return_feature:
-                results.append((record, no_ans, r.serialize()))
-            else:
-                results.append((record, no_ans))
-
-
-    
-    return results
-
-
-
-
-def generate_tfrecord(data_dir,
-                      write_fn=None, 
-                      is_training=False,
-                      return_feature=False,
-                      parallel_process=False,
-                      debug=False):
-    global count
-
-    if return_feature:
-        rs = []
-
-    i = 0
-    
-    if parallel_process:
-        cpu_count = multiprocessing.cpu_count()
-    
-        pool = Pool(cpu_count-1,initializer=init)
-        
-    tokenizer = tk = get_tokenizer()
-        
-    tot_num_no_ans = 0
-    
-    
-        
-    records = []
-    
-        
-    num_no_ans = 0
-    i += 1
-
-    jobs = ((i, c, q, is_training, return_feature) for i, c, q in gen(data_dir))
-    t0 = time()
-    results = pool.imap_unordered(work,jobs) if parallel_process else tqdm(iter(work(e, debug=debug) for e in jobs))
-    c = 0
-    for e in results:
-        for record in e:
-            if return_feature:
-                record, no_ans, r = record
-                r = tk.from_bytes(r)
-                rs.append(r)
-            else:
-                record, no_ans = record
-
-
-            records.append(record)
-
-            if no_ans:
-                num_no_ans += 1
-            c += 1
-            if c % 2500 == 0:
-                t1 = time()
-                uid, inp, start, end, p_mask, unanswerable = read(record)
-                # print(uid, tk.convert_ids_to_tokens(inp) , start, end, p_mask)
-                print('%d features (%d no ans) extracted (time: %.2f s)'%(c, num_no_ans, t1-t0))
-
-    if not return_feature:
-        random.shuffle(records)
-        with open(write_fn, 'wb') as f:
-            for record in records:
-                f.write(record)
-                f.write(b'\n')
-    tot_num_no_ans = num_no_ans
-
-    print('num has ans / num no ans : %d / %d'%(c - tot_num_no_ans, tot_num_no_ans))
-    
-    
-    if return_feature:
-        return records, rs
-    
 
 
 def chunks(l, n):
@@ -288,42 +155,6 @@ def from_records(records, batch_size = 48, half=False, shuffle=True):
         yield inp, p_mask, start, end, unanswerable
 
 # Train Utilities
-
-
-# Eval Utilities
-
-import collections
-_PrelimPrediction = collections.namedtuple(  # pylint: disable=invalid-name
-    "PrelimPrediction",
-    ["feature_index", "start_index", "end_index",
-    "start_log_prob", "end_log_prob", "this_paragraph_text",
-    "cur_null_score",
-    "label"])
-_NbestPrediction = collections.namedtuple(  # pylint: disable=invalid-name
-    "NbestPrediction", ["text", "start_log_prob", "end_log_prob","cur_null_score","label"])
-
-import math
-def _compute_softmax(scores):
-  """Compute softmax probability over raw logits."""
-  if not scores:
-    return []
-
-  max_score = None
-  for score in scores:
-    if max_score is None or score > max_score:
-      max_score = score
-
-  exp_scores = []
-  total_sum = 0.0
-  for score in scores:
-    x = math.exp(score - max_score)
-    exp_scores.append(x)
-    total_sum += x
-
-  probs = []
-  for score in exp_scores:
-    probs.append(score / total_sum)
-  return probs
 
 
 # Model Init
